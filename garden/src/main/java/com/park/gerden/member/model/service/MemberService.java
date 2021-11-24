67f5de9529f8d21918cffc019ebb7572948800c0
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-11-24 14:36:13 +0900

    Delete garden directory

--- garden/src/main/java/com/park/gerden/member/model/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.park.gerden.member.model.service;
-
-import com.park.gerden.member.model.vo.Member;
-
-public interface MemberService {
-	public int insertMember(Member member) throws Exception;
-}
